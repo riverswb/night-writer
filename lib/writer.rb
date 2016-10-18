ad08776c370f43ed1528de79cb87f9d2633feaa8
Michael Hill <michael-hill@live.com>
2016-10-18 16:35:38 -0600

    add line break

--- lib/writer.rb
+++ lib/writer.rb
@@ -15,14 +15,22 @@ class Writer
   def translate_english_to_braille
     input = read_file.chomp.gsub(/[^\p{Alnum}\p{Space}!',-.?#]/, '')
     output = translator.english_to_braille(input)
+    character_limit_160(output)
     # write_to_file_braille(output)
     writer.write_braille(output)
   end
 
-  def write_to_file_braille(output)
-    binding.pry
-    writer.write_braille(output)
+  def character_limit_160(output)
+    if output[0].length > 160
+      output.map do |string|
+        string.insert(160, "\n")
+      end
+    end
   end
+  # def write_to_file_braille(output)
+  #   binding.pry
+  #   writer.write_braille(output)
+  # end
 
   def read_file
     reader.read
