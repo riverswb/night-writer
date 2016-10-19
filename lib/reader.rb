f46c5d4e9fa22179fee7058dbb9b776d9a52d899
Michael Hill <michael-hill@live.com>
2016-10-19 08:26:43 -0600

    added character_limit_80 to reader file

--- lib/reader.rb
+++ lib/reader.rb
@@ -12,18 +12,27 @@ class Reader
   def translate_braille_to_english
     # input = read_file
     output = translator.braille_to_english(read_file)
+    character_limit_80(output)
     # write_to_file_english(output)
     writer.write_english(output)
   end
 
-  def write_to_file_english(output)
-    binding.pry
-    writer.write_english(output)
-  end
+  # def write_to_file_english(output)
+  #   binding.pry
+  #   writer.write_english(output)
+  # end
 
   def read_file
     reader.read
   end
+
+  def character_limit_80(output)
+    if output.length > 80
+      output.insert(80, "\n")
+      end
+      output
+  end
+
 
 
 end
